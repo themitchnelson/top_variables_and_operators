bc23ed3fcbdec17e1a5158c003c0d4cae4f1f967
Mitch Nelson <54041214+themitchnelson@users.noreply.github.com>
2026-03-24 11:00:20 -0400

    Finished Variables and Operators lesson

---
+++ javascript.js
@@ -0,0 +1,43 @@
+// LECTURE
+// console.log("John Otto, take 'em to the Matthews Bridge!");
+
+// let firstName = "John";
+// let lastName = "Otto";
+
+// console.log(firstName);
+// console.log(lastName);
+
+// let age = 11;
+// console.log(age); // outputs 11 to the console
+
+// age = 54;
+
+// console.log(age); // what will be output now?
+
+// // const pi = 3.14;
+// // pi = 10;
+
+// // console.log(pi); // What will be output?
+
+// console.log((3 + 2) - 76 * (1 + 1)); // What will be output?
+
+// ASSIGNMENT
+console.log(23 + 97);
+
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a = 23;
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = ((actual / max) * 100).toFixed(2) + "%";
+
+console.log(percentage);
